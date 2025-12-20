5dd129427ca73fc6f0998318913625b0edff3eb9
BBlue530 <davidlundegard@gmail.com>
2025-12-20 17:15:50 +0100

    feat(config): Added external database to app-config.yaml

--- src/Backend/config/read_app_config.py
+++ src/Backend/config/read_app_config.py
@@ -1,5 +1,6 @@
 import yaml
 from config.set_auth import aws_auth
+from config.set_database_env import database_storage_config
 from config.secret_data.get_secret_data import set_secrets_in_env
 from core.variables import app_config_path
 
@@ -10,3 +11,4 @@ def read_app_config():
 
     aws_auth(app_config)
     set_secrets_in_env(app_config)
+    database_storage_config(app_config)
--- src/Backend/database/validate_token.py
+++ src/Backend/database/validate_token.py
@@ -1,41 +1,89 @@
 import sqlite3
+import psycopg2
+import os
 from datetime import datetime
 from logs.audit_trail import audit_trail_event
 from core.variables import db_path
 
 def validate_token(audit_trail, token_key):
-    try:
-        conn = sqlite3.connect(db_path)
-        cursor = conn.cursor()
+    if os.environ.get("external_database_enabled", "False").lower() == "true":
+        try:
+            conn = psycopg2.connect(
+                dbname=os.environ.get("external_database_name"),
+                user=os.environ.get("external_database_username"),
+                password=os.environ.get("external_database_password"),
+                host=os.environ.get("external_database_host"),
+                port=5432
+            )
+            cursor = conn.cursor()
 
-        cursor.execute("""
-            SELECT ExpirationDate, Enabled, Organization 
-            FROM Key_Validation 
-            WHERE TokenKey = ?
-        """, (token_key,))
+            cursor.execute("""
+                SELECT ExpirationDate, Enabled, Organization
+                FROM Key_Validation
+                WHERE TokenKey = %s
+            """, (token_key,))
+
+            result = cursor.fetchone()
+            cursor.close()
+            conn.close()
+
+            if result is None:
+                return "Token validation: TokenKey Not Found", False
+
+            expiration_date, enabled, organization = result
+
+            if not enabled:
+                return "Token validation: TokenKey Disabled", False
+
+            if datetime.strptime(expiration_date, "%Y-%m-%d") < datetime.now():
+                return "Token validation: TokenKey Expired", False
+
+            if audit_trail is not False:
+                audit_trail_event(audit_trail, "TOKEN_VALIDATION", {
+                    "status": "valid",
+                    "expiration_date": expiration_date,
+                    "organization": organization
+                })
+
+            return organization, True
+
+        except Exception as e:
+            print(f"Error: {str(e)}")
+            return f"Token validation: Internal error {str(e)}", False
         
-        result = cursor.fetchone()
-        conn.close()
+    else:
+        try:
+            conn = sqlite3.connect(db_path)
+            cursor = conn.cursor()
+            cursor.execute("""
+                SELECT ExpirationDate, Enabled, Organization 
+                FROM Key_Validation 
+                WHERE TokenKey = ?
+            """, (token_key,))
+            
+            result = cursor.fetchone()
+            cursor.close()
+            conn.close()
 
-        if result is None:
-            return "Token validation: TokenKey Not Found", False
+            if result is None:
+                return "Token validation: TokenKey Not Found", False
 
-        expiration_date, enabled, organization = result
+            expiration_date, enabled, organization = result
 
-        if not enabled:
-            return "Token validation: TokenKey Disabled", False
+            if not enabled:
+                return "Token validation: TokenKey Disabled", False
 
-        if datetime.strptime(expiration_date, "%Y-%m-%d") < datetime.now():
-            return "Token validation: TokenKey Expired", False
-        if audit_trail is not False:
-            audit_trail_event(audit_trail, "TOKEN_VALIDATION", {
-                "status": "valid",
-                "expiration_date": expiration_date,
-                "organization": organization
-            })
+            if datetime.strptime(expiration_date, "%Y-%m-%d") < datetime.now():
+                return "Token validation: TokenKey Expired", False
+            if audit_trail is not False:
+                audit_trail_event(audit_trail, "TOKEN_VALIDATION", {
+                    "status": "valid",
+                    "expiration_date": expiration_date,
+                    "organization": organization
+                })
 
-        return organization, True
+            return organization, True
 
-    except Exception as e:
-        print(f"Error: {str(e)}")
-        return f"Token validation: Internal error {str(e)}", False
+        except Exception as e:
+            print(f"Error: {str(e)}")
+            return f"Token validation: Internal error {str(e)}", False
