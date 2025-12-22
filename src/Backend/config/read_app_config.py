a47c272e0a3c76a86fc3b3bd3e85cbf518cd2c9f
BBlue530 <davidlundegard@gmail.com>
2025-12-22 21:18:29 +0100

    fix: Fixed external secrets

--- src/Backend/config/read_app_config.py
+++ src/Backend/config/read_app_config.py
@@ -2,7 +2,7 @@ import yaml
 from config.set_auth import aws_auth
 from config.set_database_env import database_storage_config
 from config.set_scan_data_env import scan_data_storage_config
-from config.secret_data.get_secret_data import set_secrets_in_env
+from config.set_secret_data_env import secret_data_storage_config
 from core.variables import app_config_path
 
 def read_app_config():
@@ -11,6 +11,6 @@ def read_app_config():
         app_config = yaml.safe_load(f)
 
     aws_auth(app_config)
-    set_secrets_in_env(app_config)
+    secret_data_storage_config(app_config)
     database_storage_config(app_config)
     scan_data_storage_config(app_config)
--- src/Backend/config/set_secret_data_env.py
+++ src/Backend/config/set_secret_data_env.py
@@ -6,7 +6,7 @@ from config.secret_data.get_secret_data import set_secrets_in_env
 def secret_data_storage_config(app_config):
     local_secrets_storage_enabled = app_config.get("backend", {}).get("storage", {}).get("secret_data", {}).get("local", {}).get("enabled", False)
 
-    secret_manager_enabled = app_config.get("backend", {}).get("storage", {}).get("secret_data", {}).get("secret_manager", {}).get("enabled", False)
+    secret_manager_enabled = app_config.get("backend", {}).get("storage", {}).get("secret_data", {}).get("secret_manager", {}).get("aws", {}).get("enabled", False)
 
     if sum([local_secrets_storage_enabled, secret_manager_enabled]) > 1:
         print("[!] More than 1 secret data storage is enabled: [backend.storage.secret_data.]")
@@ -15,7 +15,7 @@ def secret_data_storage_config(app_config):
     if secret_manager_enabled:
         os.environ["secret_manager_enabled"] = "True"
         
-        os.environ["secret_manager_name"] = expand_env(app_config.get("backend", {}).get("storage", {}).get("secret_data", {}).get("secret_manager", {}).get("secret_manager_name", None))
+        os.environ["secret_manager_name"] = expand_env(app_config.get("backend", {}).get("storage", {}).get("secret_data", {}).get("secret_manager", {}).get("aws", {}).get("secret_manager_name", None))
         print("[+] Secret manager set in environment")
         set_secrets_in_env(app_config)
     else:
